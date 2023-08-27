5c58aee56ddc701c84b92e3049d120b6781ffe75
aajobitaiwo <jobitaiwo@gmail.com>
2023-08-26 23:12:00 -0400

    webhook test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This comment line was added to test Git Webhooh in Jenkins
+
 @SpringBootApplication
 public class DemoApplication {
 
